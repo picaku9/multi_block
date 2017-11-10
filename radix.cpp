687ceba384e66acac535150ca72eb3c28c7a2a6f
picaku9 <03kjh0421@naver.com>
2017-11-11 05:04:09 +0900

    "Radix"

--- radix.cpp
+++ radix.cpp
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-typedef struct r_node // radix_tree  nodes 
-{
-    char *key;
-    int len;
-    r_node *link;
-    r_node *next;
-    r_node(char* x, int n) : len(n), link(0), next(0)
-    {
-        key = new char[n];
-        strncpy(key,x,n);
-    }
-    ~r_node() { delete[] key; }
+typedef struct r_node // radix_tree node
+{   
+	char *key;
+	int len;
+	r_node *link;
+	r_node *next;
+	r_node(char* x, int n) : len(n), link(0), next(0)
+	{
+		key = new char[n+1];
+		strncpy(key,x,n);
+		key[n] = '\0';
+	}
+	~r_node() { delete[] key; }
 } r_node;
+
+r_node* root = NULL;
 
 int prefix(char* x, int n, char* key, int m) // length of the biggest common prefix of x and key strings 
 {
@@ -28,9 +32,9 @@ r_node* find(r_node* t, char* x, int n=0) // x key search in t tree
     if( !n ) n = strlen(x)+1;
     if( !t ) return 0;
     int k = prefix(x,n,t->key,t->len);
-    if( k==0 ) return find(t->next,x,n); // letâ<80><99>s look for the childâ<80><99>s node 
+    if( k==0 ) return find(t->next,x,n); // look for the childs node (side)
     if( k==n ) return t;
-    if( k==t->len ) return find(t->link,x+k,n-k); // letâ<80><99>s look for at the childâ<80><99>s node 
+    if( k==t->len ) return find(t->link,x+k,n-k); // look for at the child node (down)
     return 0;
 }
 
@@ -39,8 +43,9 @@ void split(r_node* t, int k) // dividing t node according to k key symbol
     r_node *p = new r_node(t->key+k,t->len-k);
     p->link = t->link;
     t->link = p;
-    char* a = new char[k];
-    strncpy(a,t->key,k);
+	char* a = (char*)malloc((k + 1) * sizeof(char));
+    strncpy(a, t->key, k);
+	a[k] = '\0';
     delete[] t->key;
     t->key = a;
     t->len = k;
@@ -63,12 +68,24 @@ r_node* insert(r_node* t, char* x, int n=0) // inserting x key in t tree
 
 int main() {
 	FILE* fp;
-	char* arg;
-	r_node* root;
-//	while(!feof(fp)){
-		fp = fopen("top-1m.csv","r");
-		fscanf(fp, "%*d,%s\n",arg);
-		root = insert(root,arg);
-//	}
+	char buffer[100];
+	int num = 0;
+	fp = fopen("top-100.csv","r");
+
+	while(!feof(fp)){
+		if (fp == NULL) return 0;
+//		printf("%d r_node made\n", num);
+		fscanf(fp, "%d,%s\n",&num, buffer);
+		root = insert(root, buffer);
+	}
+	char* target = "google.com.notarget";
+
+	if (find(root, target)) {
+		printf("There is %s\n", target);
+	}
+	else {
+		printf("There is no target \n");
+	}
+
 	return 0;
 }
